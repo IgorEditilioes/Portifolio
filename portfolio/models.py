8708293c562199cd1c5f8caeaa24a76476ec7724
Cindy Alves <igorediti@gmail.com>
2025-12-19 14:04:16 -0300

    corrigindo cloudnary

--- portfolio/models.py
+++ portfolio/models.py
@@ -1,17 +1,7 @@
-import uuid
 from django.db import models
 from django.utils.text import slugify
+from cloudinary.models import CloudinaryField
 
-def upload_imagem_principal(instance, filename):
-    ext = filename.split('.')[-1]
-    nome_projeto = instance.slug
-    return f'portfolio/{nome_projeto}/principal/{uuid.uuid4()}.{ext}'
-
-
-def upload_imagem_secundaria(instance, filename):
-    ext = filename.split('.')[-1]
-    nome_projeto = instance.projeto.slug
-    return f'portfolio/{nome_projeto}/secundarias/{uuid.uuid4()}.{ext}'
 
 class Projeto(models.Model):
     titulo = models.CharField(max_length=100)
@@ -21,8 +11,10 @@ class Projeto(models.Model):
     link = models.URLField(blank=True)
     data_criacao = models.DateTimeField(auto_now_add=True)
     slug = models.SlugField(unique=True, blank=True)
-    imagem_principal = models.ImageField(
-        upload_to=upload_imagem_principal
+
+    imagem_principal = CloudinaryField(
+        'imagem_principal',
+        folder='portfolio/principal'
     )
 
     def save(self, *args, **kwargs):
@@ -33,15 +25,17 @@ class Projeto(models.Model):
     def __str__(self):
         return self.titulo
 
-    
+
 class ImagemProjeto(models.Model):
     projeto = models.ForeignKey(
         Projeto,
         on_delete=models.CASCADE,
         related_name='imagens'
     )
-    imagem = models.ImageField(
-        upload_to=upload_imagem_secundaria
+
+    imagem = CloudinaryField(
+        'imagem_secundaria',
+        folder='portfolio/secundarias'
     )
 
     def __str__(self):
